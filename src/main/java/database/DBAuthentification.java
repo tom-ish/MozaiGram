efdd812adf71c2484660955361ff88540e5cda5b
tom-ish <tomohiro.ishiwata@gmail.com>
2017-11-06 17:57:26 +0100

    hibernate connection servlet pool

--- src/main/java/database/DBAuthentification.java
+++ src/main/java/database/DBAuthentification.java
@@ -8,23 +8,23 @@ import hibernate_entity.User;
 import utils.Persist;
 
 public class DBAuthentification {
-	
+
 	public static int getIdByUsername(String username) {
 		String hql = "from User u where u.username='"+username+"'";
-		if(Persist.OPENED_SESSION != null) {
+		if(Persist.OPENED_SESSION != null && Persist.OPENED_SESSION.isOpen()) {
 			Persist.OPENED_SESSION.beginTransaction();
 			List<User> users = Persist.OPENED_SESSION.createQuery(hql).getResultList();
 			for(User user : users)
 				if(user.getUsername().equals(username))
 					return user.getId();
-			
+
 		}
 		return -1;
 	}
-	
+
 	public static boolean existeLogin(String username) {
 		String hql = "from User";
-		if(Persist.OPENED_SESSION != null) {
+		if(Persist.OPENED_SESSION != null && Persist.OPENED_SESSION.isOpen()) {
 			Persist.OPENED_SESSION.beginTransaction();
 			List<User> users = Persist.OPENED_SESSION.createQuery(hql).getResultList();
 			for(User user : users) {
@@ -34,10 +34,10 @@ public class DBAuthentification {
 		}
 		return false;
 	}
-	
+
 	public static boolean createUser(String username, String password, String email ) {
 		User newUser = new User(username, password, email);
-		if(Persist.OPENED_SESSION != null) {			
+		if(Persist.OPENED_SESSION != null && Persist.OPENED_SESSION.isOpen()) {			
 			Persist.OPENED_SESSION.beginTransaction();
 			Persist.OPENED_SESSION.save(newUser);
 			Persist.OPENED_SESSION.getTransaction().commit();
@@ -45,11 +45,11 @@ public class DBAuthentification {
 		}
 		return Persist.KO;
 	}
-	
+
 	public static boolean isPasswordExact(String username, String password) {
 		String hql = "from User u where u.username='"+username+"'";
-		
-		if(Persist.OPENED_SESSION != null) {
+
+		if(Persist.OPENED_SESSION != null && Persist.OPENED_SESSION.isOpen()) {
 			Persist.OPENED_SESSION.beginTransaction();
 			List<User> users = Persist.OPENED_SESSION.createQuery(hql).getResultList();
 			for(User user : users)
@@ -58,26 +58,28 @@ public class DBAuthentification {
 		}
 		return false;
 	}
-	
+
 	public static String connect(String username) {
 		return DBSessionKey.addSessionKey(username);
 	}
-	
+
 	public static int logout(int userId, String sessionkey) {
 		String hql = "delete from UserSession where sessionkey = :sessionkey and userId = :userId";
-		
-		Persist.OPENED_SESSION.beginTransaction();
-		
-		int nb_row = Persist.OPENED_SESSION.createQuery(hql)
-				.setParameter("sessionkey", sessionkey)
-				.setParameter("userId", userId)
-				.executeUpdate();
-		Persist.OPENED_SESSION.getTransaction().commit();
-		
-		if(nb_row > 0)
-			return Persist.SUCCESS;
-		else if(nb_row == 0)
-			return Persist.ERROR_DB_NO_ROW_AFFECTED;
+
+		if(Persist.OPENED_SESSION != null && Persist.OPENED_SESSION.isOpen()) {
+			Persist.OPENED_SESSION.beginTransaction();
+
+			int nb_row = Persist.OPENED_SESSION.createQuery(hql)
+					.setParameter("sessionkey", sessionkey)
+					.setParameter("userId", userId)
+					.executeUpdate();
+			Persist.OPENED_SESSION.getTransaction().commit();
+
+			if(nb_row > 0)
+				return Persist.SUCCESS;
+			else if(nb_row == 0)
+				return Persist.ERROR_DB_NO_ROW_AFFECTED;
+		}
 		return Persist.ERROR_DB_LOGOUT_IMPOSSIBLE;
 	}
 }
