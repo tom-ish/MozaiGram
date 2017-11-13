18468a7ebc4782c26c723d7ee3586839ec31d08a
Reckoningevil <hugo.hourcade-boss@laposte.net>
2017-11-13 17:08:04 +0100

    search

--- src/main/webapp/js/search.js
+++ src/main/webapp/js/search.js
@@ -1,6 +1,6 @@
 function launchSearch () {
 		var word=document.getElementById("search").value;
-
+	   		console.log("Search page called...");
 			goSearch();
 	   		console.log("Searching for "+word);
 			ServerServices.getSearchResults(word);
