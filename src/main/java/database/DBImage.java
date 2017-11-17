c5fc5ea30f29f232bf3caafc2a322d0838c01c2a
datng <nguyendatci@hotmail.com>
2017-11-17 21:41:55 +0100

    add some functions for comments

--- src/main/java/database/DBImage.java
+++ src/main/java/database/DBImage.java
@@ -1,14 +1,62 @@
 package database;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import hibernate_entity.Comment;
 import hibernate_entity.Image;
 import hibernate_entity.Library;
-import hibernate_entity.User;
 import utils.Persist;
 
 public class DBImage {
-
+	
+	public static Image getImageById(int imgId) {
+		String hql = "from Image";
+		if(Persist.OPENED_SESSION != null) {
+			List<Image> imgs = Persist.OPENED_SESSION.createQuery(hql).getResultList();
+			for(Image img : imgs) {
+				if(img.getId() == imgId)
+					return img;
+			}
+		}
+		return null;
+	}
+	
+	public static List<Image> getImageFromUserId(int userid){
+		String hql = "from Image image where image.id in "
+				+ "(select images_id from library_images where library_id = "
+				+ "(select id from library where userid ='" + userid +"'))";
+		
+		if(Persist.OPENED_SESSION != null) {
+			return Persist.OPENED_SESSION.createQuery(hql).getResultList();
+		}
+		return null;
+	}
+	
+	public static boolean existeImageId(int imgId) {
+		String hql = "from Image";
+		if(Persist.OPENED_SESSION != null) {
+			List<Image> imgs = Persist.OPENED_SESSION.createQuery(hql).getResultList();
+			for(Image img : imgs) {
+				if(img.getId() == imgId)
+					return true;
+			}
+		}
+		return false;
+	}
+	
+	public static int addComment(Image img, String txt) {
+		Comment comment = new Comment(img, txt);
+		
+		if(Persist.OPENED_SESSION != null) {			
+			Persist.OPENED_SESSION.beginTransaction();
+			Persist.OPENED_SESSION.save(comment);
+			Persist.OPENED_SESSION.getTransaction().commit();
+			return Persist.SUCCESS;
+		}
+		return Persist.ERROR;
+	}
+	
 	public static int addImage(String imgPath) {
 		Image img = new Image(imgPath);
 		if(Persist.OPENED_SESSION != null) {
@@ -21,26 +69,19 @@ public class DBImage {
 	}
 
 	public static int addImageToLibrary(int userId, int imgId) {
-		User user = DBAuthentification.getUserById(userId);
-		Image img = DBImage.getImgFromId(imgId);
-		if(user != null && img != null) {
-			Library library = new Library();
-			library.setUser(user);
-			library.getImages().add(img);
-			if(Persist.OPENED_SESSION != null) {
-				Persist.OPENED_SESSION.beginTransaction();
-				Persist.OPENED_SESSION.save(library);
-				Persist.OPENED_SESSION.getTransaction().commit();
-				Persist.OPENED_SESSION.close();
-			}
-			return library.getId();
+		Library library = new Library();
+		if(Persist.OPENED_SESSION != null) {
+			Persist.OPENED_SESSION.beginTransaction();
+			Persist.OPENED_SESSION.save(library);
+			Persist.OPENED_SESSION.getTransaction().commit();
+			Persist.OPENED_SESSION.close();
 		}
-		return -1;
+		return library.getId();
 	}
-
+	
 	public static String getPathFromImgId(int id) {
 		String hql = "from Image";
-
+		
 		if(Persist.OPENED_SESSION != null) {
 			List<Image> images = Persist.OPENED_SESSION.createQuery(hql).getResultList();
 			Persist.OPENED_SESSION.close();
@@ -51,19 +92,7 @@ public class DBImage {
 		}
 		return null;
 	}
-
-	public static Image getImgFromId(int id) {
-		String hql = "from Image";
-
-		if(Persist.OPENED_SESSION != null) {
-			List<Image> images = Persist.OPENED_SESSION.createQuery(hql).getResultList();
-			Persist.OPENED_SESSION.close();
-			for(Image img : images) {
-				if(img.getId() == id)
-					return img;
-			}
-		}
-		return null;
-	}
-
+	
+	
+	
 }
